111e75cec9b94d657e9510d50c17e38f9a2af04f
amanduke <65379991+amanduke@users.noreply.github.com>
2021-02-18 17:06:54 -0500

    stopped loop

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src='/videos/video-1.mp4' muted playsInline/>
       <h1>Alexis Manduke</h1>
       <p>Web Developer And Designer</p>
     </div>
